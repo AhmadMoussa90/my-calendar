3be7eab8c2f1654b9eb7671c7e7359dc7ecb90b0
AhmadMOUSSA <ahmad.moussa.lb@gmail.com>
2022-01-14 23:43:16 +0100

    [backend] ADD mongoDB connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running...")
-});
+/* eslint-disable no-console */
+mongoose
+  .connect(
+    "mongodb+srv://admin:adminadmin@constraintsolver-db.tw6br.mongodb.net/my_calendar?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful...");
+    console.log("Server is running...");
+  })
+  .catch((err) => console.log(err));
+/* eslint-disable no-console */
