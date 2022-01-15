16d30124b506d1d987e0deec261fe8004ab127f9
AhmadMOUSSA <ahmad.moussa.lb@gmail.com>
2022-01-15 03:14:45 +0100

    [backend] ADD new roots - ADD partner & createPartner roots - ADD room & createRoom roots - ADD timeSlot && createTimeSlot roots

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -1,8 +1,23 @@
 import express from "express";
 import CompanyModel, { Company } from "../models/company";
+import PartnerModel, { Partner } from "../models/partner";
+import RoomModel, { Room } from "../models/room";
+import TimeSlotModel, { TimeSlot } from "../models/timeSlot";
 
 type CompanyRequest = {
   companyInput: Company;
+};
+
+type PartnerRequest = {
+  partnerInput: Partner;
+};
+
+type RoomRequest = {
+  roomInput: Room;
+};
+
+type TimeSlotRequest = {
+  timeSlotInput: TimeSlot;
 };
 
 type ID = {
@@ -34,6 +49,87 @@ export default {
     return {
       _id: company._id.toString(),
       name: company.name
+    };
+  },
+
+  createPartner: async function( { partnerInput }: PartnerRequest, req:express.Request ) {
+    const existingPartner = await PartnerModel.findOne({ name: partnerInput.name });
+    if (existingPartner) {
+      const error = new Error('Partner exists already!');
+      throw error;
+    }
+
+    const partner = new PartnerModel({
+      name: partnerInput.name,
+    });
+    const createdPartner = await partner.save();
+
+    return { _id: createdPartner._id.toString(), name: createdPartner.name };
+  },
+
+  partner: async function({ id }: ID) {
+    const partner = await PartnerModel.findById(id);
+    if (!partner) {
+      const error = new Error('No partner found!');
+      throw error;
+    }
+    return {
+      _id: partner._id.toString(),
+      name: partner.name
+    };
+  },
+
+  createRoom: async function( { roomInput }: RoomRequest, req:express.Request ) {
+    const existingRoom = await RoomModel.findOne({ name: roomInput.name });
+    if (existingRoom) {
+      const error = new Error('Room exists already!');
+      throw error;
+    }
+
+    const room = new RoomModel({
+      name: roomInput.name,
+    });
+    const createdRoom = await room.save();
+
+    return { _id: createdRoom._id.toString(), name: createdRoom.name };
+  },
+
+  room: async function({ id }: ID) {
+    const room = await RoomModel.findById(id);
+    if (!room) {
+      const error = new Error('No room found!');
+      throw error;
+    }
+    return {
+      _id: room._id.toString(),
+      name: room.name
+    };
+  },
+
+  createTimeSlot: async function( { timeSlotInput }: TimeSlotRequest, req:express.Request ) {
+    const existingTimeSlot = await TimeSlotModel.findOne({ startDate: timeSlotInput.startDate });
+    if (existingTimeSlot) {
+      const error = new Error('Time slot exists already!');
+      throw error;
+    }
+
+    const timeSlot = new TimeSlotModel({
+      startDate: timeSlotInput.startDate,
+    });
+    const createdTimeSlot = await timeSlot.save();
+
+    return { _id: createdTimeSlot._id.toString(), startDate: createdTimeSlot.startDate };
+  },
+
+  timeSlot: async function({ id }: ID) {
+    const timeSlot = await TimeSlotModel.findById(id);
+    if (!timeSlot) {
+      const error = new Error('No time slot found!');
+      throw error;
+    }
+    return {
+      _id: timeSlot._id.toString(),
+      startDate: timeSlot.startDate
     };
   },
 };
--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -6,16 +6,49 @@ export default buildSchema(`
         name: String!
     }
 
+    type Partner {
+      _id: ID!
+      name: String!
+    }
+
+    type Room {
+      _id: ID!
+      name: String!
+    }
+
+    type TimeSlot {
+      _id: ID!
+      startDate: String!
+    }
+
     input CompanyInput {
         name: String!
     }
 
+    input PartnerInput {
+      name: String!
+    }
+
+    input RoomInput {
+      name: String!
+    }
+
+    input TimeSlotInput {
+      startDate: String!
+    }
+
     type RootQuery {
         company(id: ID!): Company!
+        partner(id: ID!): Partner!
+        room(id: ID!): Room!
+        timeSlot(id: ID!): TimeSlot!
     }
 
     type RootMutation {
         createCompany(companyInput: CompanyInput): Company!
+        createPartner(partnerInput: PartnerInput): Partner!
+        createRoom(roomInput: RoomInput): Room!
+        createTimeSlot(timeSlotInput: TimeSlotInput): TimeSlot!
     }
 
     schema {
