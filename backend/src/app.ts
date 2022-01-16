ce755ab8777bfd4390db6b47cfc552a7dd4a12de
AhmadMOUSSA <ahmad.moussa.lb@gmail.com>
2022-01-16 03:49:22 +0100

    [backend] ADD CORS configurations

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
