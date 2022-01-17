fddd5eac7f310c689165062bf97ccb39179f325c
AhmadMOUSSA <ahmad.moussa.lb@gmail.com>
2022-01-17 07:51:42 +0100

    [frontend] UPDATE Calendar's props

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -1,4 +1,9 @@
-const Calendar: React.FC<{}> = () => {
+import { Room, TimeSlot } from "../../models";
+
+const Calendar: React.FC<{
+  roomsItems: Room[];
+  timeSlotsItems: TimeSlot[];
+}> = () => {
   return <h2>My Calendar</h2>;
 };
 
