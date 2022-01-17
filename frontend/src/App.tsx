373174cc97ed95e90ed3670ed741a708987b91af
AhmadMOUSSA <ahmad.moussa.lb@gmail.com>
2022-01-17 07:53:55 +0100

    [frontend] UPDATE App.tsx - ADD Calendar component - IMPROVE app performance by using useEffect hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import CompanyList from "./components/companies/CompanyList";
 import MainNavigation from "./components/shared/Navigation/MainNavigation";
 import UserList from "./components/users/UserList";
-import { Company, User } from "./models";
+import { Company, Room, TimeSlot, User } from "./models";
 import { fetchData } from "./utils/intercall";
-import { COMPANIES, COMPANY_USERS, USER_LOGIN } from "./utils/queries";
+import {
+  COMPANIES,
+  COMPANY_ROOMS,
+  COMPANY_USERS,
+  TIMESLOTS,
+  USER_LOGIN,
+} from "./utils/queries";
 import Calendar from "./components/Calendar/Calendar";
 
 function App() {
@@ -17,6 +23,74 @@ function App() {
   const [user, setUser] = useState<User>();
   const [isAuth, setIsAuth] = useState<Boolean>(false);
   const [warningMessage, setWarningMessage] = useState<String | null>(null);
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const [timeSlots, setTimeSlots] = useState<TimeSlot[]>([]);
+
+  useEffect(() => {
+    const loadCompanies = async () => {
+      const graphqlQuery = {
+        query: COMPANIES,
+      };
+
+      const resData = await fetchData(graphqlQuery);
+      if (resData.errors) throw new Error("Fetching companies failed!");
+      setCompanies(resData.data.companies);
+    };
+
+    // company not selected
+    if (companies.length === 0) loadCompanies();
+  }, [companies]);
+
+  useEffect(() => {
+    const loadCompanyUsers = async () => {
+      const graphqlQuery = {
+        query: COMPANY_USERS,
+        variables: {
+          id: companyID,
+        },
+      };
+
+      const resData = await fetchData(graphqlQuery);
+      if (resData.errors) throw new Error("Fetching users failed!");
+      setUsers(resData.data.companyUsers);
+    };
+
+    // company selected
+    if (companyID && users.length === 0) loadCompanyUsers();
+  }, [companyID, users]);
+
+  useEffect(() => {
+    const loadRooms = async () => {
+      const graphqlQuery = {
+        query: COMPANY_ROOMS,
+        variables: {
+          id: companyID,
+        },
+      };
+
+      const resData = await fetchData(graphqlQuery);
+      if (resData.errors) throw new Error("Fetching rooms failed!");
+      setRooms(resData.data.CompanyRooms);
+    };
+
+    // isAuth
+    if (isAuth && rooms && rooms.length === 0) loadRooms();
+  }, [companyID, isAuth, rooms]);
+
+  useEffect(() => {
+    const loadTimeSlots = async () => {
+      const graphqlQuery = {
+        query: TIMESLOTS,
+      };
+
+      const resData = await fetchData(graphqlQuery);
+      if (resData.errors) throw new Error("Fetching timeslots failed!");
+      setTimeSlots(resData.data.timeSlots);
+    };
+
+    // isAuth
+    if (isAuth && timeSlots && timeSlots.length === 0) loadTimeSlots();
+  }, [isAuth, timeSlots]);
 
   const onSelectCompanyHandler = (id: String | null) => {
     setCompanyID(id);
@@ -53,35 +127,6 @@ function App() {
     setIsAuth(true);
   };
 
-  const loadCompanies = async () => {
-    const graphqlQuery = {
-      query: COMPANIES,
-    };
-
-    const resData = await fetchData(graphqlQuery);
-    if (resData.errors) throw new Error("Fetching companies failed!");
-    setCompanies(resData.data.companies);
-  };
-
-  const loadCompanyUsers = async () => {
-    const graphqlQuery = {
-      query: COMPANY_USERS,
-      variables: {
-        id: companyID,
-      },
-    };
-
-    const resData = await fetchData(graphqlQuery);
-    if (resData.errors) throw new Error("Fetching users failed!");
-    setUsers(resData.data.companyUsers);
-  };
-
-  // company not selected
-  if (companies.length === 0) loadCompanies();
-
-  // company selected
-  if (companyID && users.length === 0) loadCompanyUsers();
-
   return (
     <Fragment>
       <div className="container">
@@ -104,7 +149,9 @@ function App() {
                   returnBackHandler={returnBackHandler}
                 />
               )}
-              {isAuth && <Calendar />}
+              {isAuth && (
+                <Calendar roomsItems={rooms} timeSlotsItems={timeSlots} />
+              )}
             </main>
           </div>
         </div>
