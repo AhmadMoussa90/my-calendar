c55e317f5bccb77ab90057cbee300477cfcf5b16
AhmadMOUSSA <ahmad.moussa.lb@gmail.com>
2022-01-19 11:37:02 +0100

    [frontend] FIX username & logout appearance

--- frontend/src/components/shared/Navigation/MainNavigation.tsx
+++ frontend/src/components/shared/Navigation/MainNavigation.tsx
@@ -5,7 +5,11 @@ import MainHeader from "./MainHeader";
 
 import classes from "./MainNavigation.module.css";
 
-const MainNavigation: React.FC<{ username?: String }> = (props) => {
+const MainNavigation: React.FC<{
+  isAuth: Boolean;
+  username?: String;
+  onLogOut: () => void;
+}> = (props) => {
   const RightSide = (
     <div className={`row h-100 ${classes.mainNavigationTitle}`}>
       <div className={`col`}>
@@ -14,27 +18,28 @@ const MainNavigation: React.FC<{ username?: String }> = (props) => {
     </div>
   );
 
-  const CenterSide = props.username ? (
+  const CenterSide = props.username && props.isAuth && (
     <div className={`row h-100 ${classes.mainNavigationTitle}`}>
       <div className={`col d-flex`}>
         <h2>{props.username}</h2>
       </div>
     </div>
-  ) : null;
-
-  const LeftSide = props.username ? (
-    <div className={`row h-100 ${classes.mainNavigationTitle}`}>
-      <div className="col d-flex justify-content-center">
-        <Button> LogOut </Button>
-      </div>
-    </div>
-  ) : (
-    <div className={`row h-100 ${classes.mainNavigationTitle}`}>
-      <div className={`col ${classes.mainNavigationTitle}`}>
-        <h2>Unloged</h2>
-      </div>
-    </div>
   );
+
+  const LeftSide =
+    props.username && props.isAuth ? (
+      <div className={`row h-100 ${classes.mainNavigationTitle}`}>
+        <div className="col d-flex justify-content-center">
+          <Button onClick={props.onLogOut}> LogOut </Button>
+        </div>
+      </div>
+    ) : (
+      <div className={`row h-100 ${classes.mainNavigationTitle}`}>
+        <div className={`col ${classes.mainNavigationTitle}`}>
+          <h2>Unloged</h2>
+        </div>
+      </div>
+    );
 
   return (
     <React.Fragment>
